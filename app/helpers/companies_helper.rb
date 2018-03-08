fbb4ee9f47172eae4f0a163a7e8a503eca5392f1
allanscu <ateruel@gmail.com>
2018-03-07 22:27:09 -0800

    cleaned up company helper and show

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -2,9 +2,9 @@ module CompaniesHelper
 
 	def adjusted_datetime
 		if Time.now.dst?
-			(@company.items.order(release_date: :asc).last.release_date - 7.hours).to_datetime
+			(@company.items.order(release_date: :asc).last.release_date + 17.hours).to_datetime
 		else
-			(@company.items.order(release_date: :asc).last.release_date - 8.hours).to_datetime
+			(@company.items.order(release_date: :asc).last.release_date + 16.hours).to_datetime
 		end
 	end
 	
